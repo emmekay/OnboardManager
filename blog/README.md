32e535bd08c6b79be66855b44e56d3195abf0d5b
Emilia Kobina <kobinaee@cardiff.ac.uk>
2021-02-09 19:54:42 +0000

    Add thankyou.html for redirect after registration and animate text

---
+++ blog/README.md
@@ -0,0 +1,2 @@
+# Sourcing
+# http://www.pickatutorial.com/tutorial/html/simpletextanimation.htm - Used for finding coding for text animation on thankyou.html
--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, url_for
-from blog import app
+from flask import render_template, url_for, request, redirect 
+from blog import app, db
 from blog.models import User, Post
+from blog.forms import RegistrationForm 
 
 @app.route("/")
 @app.route("/home")
@@ -13,6 +14,9 @@ def home():
 def about():
     return render_template('about.html', title='About Me')
 
+@app.route("/thankyou")
+def thankyou():
+    return render_template('thankyou.html', title='Thank You!')
 
 @app.route("/cv")
 def cv():
@@ -22,3 +26,14 @@ def cv():
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if request.method == 'POST':
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('thankyou'))
+    return render_template('register.html', title='Register', form=form)
