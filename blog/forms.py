996cca78bd3e10e02c8e4b59fa126cf74323116e
Emilia Kobina <kobinaee@cardiff.ac.uk>
2021-02-13 15:54:51 +0000

    made changes to correct registration error.  Deleted columns for name in table

--- blog/forms.py
+++ blog/forms.py
@@ -4,8 +4,6 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
-    firstname = StringField('First Name', validators=[DataRequired(), Length(min=2, max=15)])
-    lastname = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
--- blog/models.py
+++ blog/models.py
@@ -12,35 +12,33 @@ class Post(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
-def __repr__(self):
-    return f"Post('{self.date}', '{self.title}', '{self.content}')"
+    def __repr__(self):
+        return f"Post('{self.date}', '{self.title}', '{self.content}')"
 
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) 
-    firstname = db.Column(db.String(15), unique=True, nullable=False)
-    lastname = db.Column(db.String(15), unique=True, nullable=False)
-    username = db.Column(db.String(15), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-    password = db.Column(db.String(60), nullable=False) 
-    post = db.relationship('Post', backref='user', lazy=True)
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(15), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password_hash = db.Column(db.String(128))
+  password = db.Column(db.String(60), nullable=False)
+  post = db.relationship('Post', backref='user', lazy=True)
 
 
-    def __repr__(self): 
-        return f"User('{self.username}', '{self.email}')"
+  def __repr__(self): 
+    return f"User('{self.username}', '{self.email}')"
 
-    @property
-    def password(self):
-        raise AttributeError('password is not a readable attribute')
+  @property
+  def password(self):
+    raise AttributeError('password is not a readable attribute')
 
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
+  @password.setter
+  def password(self, password):
+    self.password_hash = generate_password_hash(password)
 
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash,password)
+  def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
 
 @login_manager.user_loader
 def load_user(user_id):
-        return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
