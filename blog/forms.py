25cb22f9ccffcc5034ada295abe6b3e155124cdc
Emilia Kobina <kobinaee@cardiff.ac.uk>
2021-02-21 20:42:11 +0000

    start sheet 3

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp 
+from blog.models import User 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
@@ -11,6 +12,11 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+    def validaate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user: 
+            raise ValidationError('Username already exist. Please choos a different one.')
+        
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(),Email()])
     password = PasswordField('Password',validators=[DataRequired()])
